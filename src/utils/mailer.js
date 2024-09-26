f1299a90ab27e98755e1a5183698ea42f81a0ad6
IamArbabMemon <139270412+IamArbabMemon@users.noreply.github.com>
2024-09-26 07:41:22 -0700

    'commit'

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,69 +1,64 @@
-// import nodemailer from 'nodemailer'
-// const transporter = nodemailer.createTransport({
-//     service:"gmail",
-//     host: 'smtp.gmail.com',
-//     auth: {
-//         user: process.env.MAILER_EMAIL,
-//         pass: process.env.MAILER_PASS
-//     }
-// });
+import nodemailer from 'nodemailer'
 
-// const sendWelcomeMail = async (user:{username:string,email:string})=>{
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: 'smtp.gmail.com',
+    auth: {
+        user: process.env.MAILER_EMAIL,
+        pass: process.env.MAILER_PASS
+    }
+});
+
+const sendWelcomeMail = async (user) => {
+    let textToSend = "DEAR [User] WELCOME TO THE FAMILY OF OUR CUSTOMERS. WE WISH YOU A GREAT JOURNEY WITH US!";
+    textToSend = textToSend.replace("[User]", user.username);
+
+    try {
+        const info = await transporter.sendMail({
+            from: {
+                name: `UNIQUE LEARNING PLATFORM Team`,
+                address: process.env.MAILER_EMAIL
+            }, // sender address
+            to: user.email, // list of receivers
+            subject: "WELCOME EMAIL", // Subject line
+            text: textToSend, // plain text body
+            html: "" // html body
+        });
+
+        return info;
+
+    } catch (err) {
+        console.log("FAILED TO SEND EMAIL", err);
+        return null;
+    }
+};
+
+const sendOTPMail = async (user, linkToProvide) => {
+    let textToSend = `Click here to reset password: ${linkToProvide}`;
     
-//     let textToSend = "DEAR [User] WELCOME TO THE UNIQUE LEARNING PLATFORM . WE WISH YOU A GREAT JOURNEY WITH US !";
-//     textToSend = textToSend.replace("[User]",user.username);
-    
-//     try{
-        
-//         const info = await transporter.sendMail({
-//             from: {
-//                 name:`UNIQUE LEARNING PLATFORM Team`,
-//                 address:'arbabhere41@gmail.com'
-//         }, // sender address
-//             to: user.email, // list of receivers
-//             subject: "WELCOME EMAIL", // Subject line
-//             text: textToSend, // plain text body
-//             html:""// html body
-//           });
+    try {
+        const info = await transporter.sendMail({
+            from: {
+                name: `UNIQUE LEARNING PLATFORM Team`,
+                address: process.env.MAILER_EMAIL
+            }, // sender address
+            to: user.email, // list of receivers
+            subject: "RESET PASSWORD REQUEST", // Subject line
+            text: textToSend, // plain text body
+            html: "" // html body
+        });
 
-//           return info;
-          
-//       }catch(err){
-//         console.log("FAILED TO SEND EMAIL");
-//         return null;
-//     }
+        return info;
 
-// };
+    } catch (err) {
+        console.log("FAILED TO SEND EMAIL", err);
+        return null;
+    }
+};
 
-// const sendOTPMail = async (user:{username:string,email:string},linkToProvide :string)=>{
-    
-//     let textToSend = `Click here to reset password : ${linkToProvide}`;
-//     textToSend = textToSend.replace("[User]",user.username);
-    
-//     try{
-        
-//         const info = await transporter.sendMail({
-//             from: {
-//                 name:`UNIQUE LEARNING PLATFORM Team`,
-//                 address:'arbabhere41@gmail.com'
-//         }, // sender address
-//             to: user.email, // list of receivers
-//             subject: "RESET PASSWORD REQUEST", // Subject line
-//             text: textToSend, // plain text body
-//             html:""// html body
-//           });
-
-//           return info;
-          
-//       }catch(err){
-//         console.log("FAILED TO SEND EMAIL" , err);
-//         return null;
-//     }
-
-// };
+module.exports = {
+    sendOTPMail,
+    sendWelcomeMail
+};
 
 
-// export {
-//     sendOTPMail,
-//     sendWelcomeMail
-// }
