3f7b87e5d4efab07a260448a5c868eda0ad92d0e
IamArbabMemon <139270412+IamArbabMemon@users.noreply.github.com>
2024-09-27 16:18:34 -0700

    'commit'

---
+++ src/models/tempForInitialRegistration.model.js
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose';
+
+const tempModelSchema = new mongoose.Schema({
+
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+      },
+      mobileNumber: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        validate: {
+          validator: function (v) {
+            return /\d{10}/.test(v);  // Indian 10-digit mobile number validation
+          },
+          message: props => `${props.value} is not a valid mobile number!`
+        },
+
+      },
+
+      address: {
+        country: { type: String},
+        state:{type:String},
+        city: { type: String}
+        
+      },
+
+      role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+      },
+
+      OTP:{
+        type:String
+      }
+    
+
+
+},{timestamps:true});
+
+const tempInitialRegistrationModel = mongoose.model('temp-initial-registration', tempModelSchema);
+
+export{
+   tempInitialRegistrationModel
+};
+
+
